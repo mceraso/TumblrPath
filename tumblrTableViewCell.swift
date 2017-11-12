0214acc36b7ba869f443cc97d4f626481bafb65b
Michael Ceraso <mceraso@fordham.edu>
2017-11-11 23:08:48 -0600

    CocoaPods and basic TableView Setup

---
+++ tumblrTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  tumblrTableViewCell.swift
+//  TumblrPath
+//
+//  Created by Michael Ceraso on 11/11/17.
+//  Copyright © 2017 Michael Ceraso. All rights reserved.
+//
+
+import UIKit
+
+class tumblrTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tumblrImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
